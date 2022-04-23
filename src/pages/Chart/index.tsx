780c36f0323918c344442e29bc539650bec2679a
Valentino <valentino@secret-source.eu>
2022-04-23 17:38:16 +0200

    Remove NivoChart component

--- src/pages/Chart/index.tsx
+++ src/pages/Chart/index.tsx
@@ -1,10 +1,8 @@
 import ApexBarChart from "../../components/ApexBarChart";
-import NivoChart from "../../components/NivoChart";
 
 export default function Chart() {
   return (
     <div style={{ height: 400 }}>
-      <NivoChart />
       <ApexBarChart />
     </div>
   );
